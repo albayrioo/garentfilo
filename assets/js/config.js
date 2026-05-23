c32bd508c56d664e349fe376edfc2b86b1a015da
albayrioo <ozkagsr@gmail.com>
2026-05-23 14:12:57 +0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  companyName: 'GARENT FİLO',
+  phoneDisplay: '905521211575',
+  phoneRaw: '905521211575',
+  whatsappRaw: '905078162654',
+  whatsappMessage: 'Merhaba, Zenta Filo üzerinden araç rezervasyonu yapmak istiyorum.',
+  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
+  workingHours: '7/24 rezervasyon ve teslimat desteği'
+};
