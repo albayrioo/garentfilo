d5b23b3dc5bc414c24dedddd6cf37a5944659594
albayrioo <ozkagsr@gmail.com>
2026-05-26 13:28:32 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ window.SITE_CONFIG = {
   phoneDisplay: '905543423514',
   phoneRaw: '905543423514',
   whatsappRaw: '905530752552',
-  whatsappMessage: 'Merhaba, Zenta Filo üzerinden araç rezervasyonu yapmak istiyorum.',
+  whatsappMessage: 'Merhaba, Garent Filo Araç Rezervasyonu Yapmak İstiyorum.',
   locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
   workingHours: '7/24 rezervasyon ve teslimat desteği'
 };
